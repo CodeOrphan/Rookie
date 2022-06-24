c0a6e25c27fb3b91010e2f98f4766993233fc64b
CodeOrphan <76989989+CodeOrphan@users.noreply.github.com>
2022-06-25 00:26:08 +0800

    控制器

--- Trick/Assets/Scripts/PlayerController.cs
+++ Trick/Assets/Scripts/PlayerController.cs
@@ -1,28 +1,181 @@
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+using MoreMountains.Tools;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float Speed = 10f;
+    private Rigidbody _rigidbody;
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+    private BoxCollider _boxCollider;
+    
+    public Vector3 Movement;
 
-    void Update()
+    public GameObject SpriteModel;
+    
+    public const string AxisHorizontal = "Horizontal";
+    public const string AxisVertical = "Vertical";
+
+    public const float InputThreshold = 0.1f;
+
+    public const float SmallValue = 0.0001f;
+
+    public bool SmoothMovement = true;
+
+    public float MovementSpeed = 5f;
+
+    public float MovementFactor = 15;
+    
+    public HashSet<int> _animatorParameters { get; set; }
+
+
+    private int _idleParameter;
+    private int _walkParameter;
+
+    public Vector3 Speed => _rigidbody.velocity;
+
+    private Vector3 _lastPosition;
+    private Vector3 _positionTolerance;
+    private Transform _mainCamera;
+
+    public void Start()
     {
-        if(Input.GetKey(KeyCode.RightArrow))
+        _rigidbody = GetComponent<Rigidbody>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+        _boxCollider = GetComponent<BoxCollider>();
+        
+        _animatorParameters = new HashSet<int>();
+        if (Camera.main is not null) _mainCamera = Camera.main.transform;
+
+        Physics2D.autoSyncTransforms = true;
+        RegisterAnimatorParameter("Idle", AnimatorControllerParameterType.Bool, out _idleParameter);
+        RegisterAnimatorParameter("Walk", AnimatorControllerParameterType.Bool, out _walkParameter);
+    }
+
+    public virtual void RegisterAnimatorParameter(string parameterName, AnimatorControllerParameterType parameterType,
+        out int parameter)
+    {
+        parameter = Animator.StringToHash(parameterName);
+
+        if (_animator == null)
         {
-            transform.Translate(new Vector3(Speed * Time.deltaTime, 0, 0));
+            return;
         }
-        if(Input.GetKey(KeyCode.LeftArrow))
+
+        if (_animator.MMHasParameterOfType(parameterName, parameterType))
         {
-            transform.Translate(new Vector3(-Speed * Time.deltaTime, 0, 0));
+            _animatorParameters.Add(parameter);
         }
-        if(Input.GetKey(KeyCode.DownArrow))
+    }
+
+    private bool IsIdle;
+    private bool IsWalk;
+
+    public void Update()
+    {
+        MMAnimatorExtensions.UpdateAnimatorBool(_animator, _idleParameter, IsIdle,
+            _animatorParameters, true);
+        MMAnimatorExtensions.UpdateAnimatorBool(_animator, _walkParameter, IsWalk,
+            _animatorParameters, true);
+
+        _positionTolerance = transform.position - _lastPosition;
+        
+        UpdateMovement();
+        
+        Vector3 dir = _mainCamera.position - transform.position;
+
+        SpriteModel.transform.LookAt(dir);
+    }
+
+    public void LateUpdate()
+    {
+        _lastPosition = transform.position;
+    }
+
+    private Vector3 _normalizedHorizontalSpeed;
+
+    public void UpdateMovement()
+    {
+        if (string.IsNullOrEmpty(AxisHorizontal) || string.IsNullOrEmpty(AxisVertical))
         {
-            transform.Translate(new Vector3(0,0,-Speed * Time.deltaTime));
+            return;
         }
-        if(Input.GetKey(KeyCode.UpArrow))
+
+        Movement.x = Input.GetAxis(AxisHorizontal);
+        Movement.z = Input.GetAxis(AxisVertical);
+
+        _normalizedHorizontalSpeed = Movement;
+        if (Movement.x > InputThreshold)
         {
-            transform.Translate(new Vector3(0, 0, Speed * Time.deltaTime));
+            if (SpriteModel != null)
+            {
+                if (SpriteModel.transform.localScale.x < 0)
+                {
+                    SpriteModel.transform.localScale =
+                        Vector3.Scale(SpriteModel.transform.localScale, new Vector3(-1, 1, 1));
+                }
+            }
+            else if (transform.localScale.x < 0)
+            {
+                
+                transform.localScale =
+                    Vector3.Scale(transform.localScale, new Vector3(-1, 1, 1));
+            }
         }
+        else if (Movement.x < -InputThreshold)
+        {
+            if (SpriteModel != null)
+            {
+                if (SpriteModel.transform.localScale.x > 0)
+                {
+                    SpriteModel.transform.localScale =
+                        Vector3.Scale(SpriteModel.transform.localScale, new Vector3(-1, 1, 1));
+                }
+            }
+            else if (transform.localScale.x > 0)
+            {
+                transform.localScale =
+                    Vector3.Scale(transform.localScale, new Vector3(-1, 1, 1));
+            }
+        }
+
+        Vector3 realHorizontalForce;
+        if (SmoothMovement)
+        {
+            realHorizontalForce = Vector3.Lerp(_rigidbody.velocity, _normalizedHorizontalSpeed * MovementSpeed,
+                Time.deltaTime * MovementFactor);
+        }
+        else
+        {
+            realHorizontalForce = _normalizedHorizontalSpeed * MovementSpeed;
+        }
+
+        if (Mathf.Abs(_normalizedHorizontalSpeed.x) > SmallValue ||
+            Mathf.Abs(_normalizedHorizontalSpeed.y) > SmallValue)
+        {
+            IsWalk = true;
+            IsIdle = false;
+        }
+
+        if (Mathf.Abs(_normalizedHorizontalSpeed.x) < SmallValue &&
+            Mathf.Abs(_normalizedHorizontalSpeed.y) < SmallValue)
+        {
+            IsWalk = false;
+            IsIdle = true;
+        }
+        
+        _rigidbody.velocity = realHorizontalForce;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        
     }
 }
